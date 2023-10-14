c4a3baaf26179d7e990383b2b8b278e0c65316b5
anhngocdohaha <anhngocdohaha@gmail.com>
2023-10-15 05:37:52 +0700

    anhngocdo

--- App.js
+++ App.js
@@ -1,43 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-// // export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./src/general/SplashScreen";
 import Login from "./src/general/Login";
 import SignUp from "./src/general/SignUp";
-import Onboarding from "./src/general/Onboarding";
-import SetupInterests from "./src/general/SetupInterests";
-
+import Profile from "./src/user/profile"
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="profile" component={Profile} />
+      {/* <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Login" component={Login} /> */}
+    </Stack.Navigator>
     </NavigationContainer>
   );
 }
