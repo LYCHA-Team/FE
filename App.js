38a39aacfb2b57bf70eee4517c3161112620d8ff
nhatha3788 <ha.hoang3788ha@hcmut.edu.vn>
2023-10-14 19:23:15 +0700

    set up routes for splashscreen and user login, signup.

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+
+// // export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+// import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "./src/general/SplashScreen";
+import Login from "./src/general/Login";
+import SignUp from "./src/general/SignUp";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/general/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default Login = () => {
+  return (
+    <View>
+      <Text>Login text</Text>
+      <Image
+        source={{
+          uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+    </View>
+  );
+};
